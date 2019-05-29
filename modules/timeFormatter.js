70972114dd8e7ffc8c653ee8fca17ebea74d694e
jakub-kopec <agh.jakub@gmail.com>
2019-05-29 21:40:15 +0200

    13.4 ready

--- modules/timeFormatter.js
+++ modules/timeFormatter.js
@@ -1,13 +1,10 @@
 var os = require('os');
 
-function formatTime() {
-
-    var uptime = os.uptime();
-    console.log('diff', (uptime % 3600))
-    if ((uptime / 3600).toFixed(0) !== '0') {
-        return (uptime / 3600).toFixed(0) + 'h, ' + ((uptime % 3600) / 60).toFixed(0) + 'min and ' + (uptime % 60) + ' seconds'
-    } else if (((uptime % 3600) / 60).toFixed(0) !== '0') {
-        return ((uptime % 3600) / 60).toFixed(0) + ' min and ' + (uptime % 60) + ' seconds'
+function formatTime(uptime) {
+    if (uptime > 3600) {
+        return Math.floor(uptime / 3600) + 'h, ' + Math.floor((uptime % 3600) / 60) + 'min and ' + (uptime % 60) + ' seconds'
+    } else if (((uptime % 3600) / 60) !== '0') {
+        return Math.floor((uptime % 3600) / 60) + ' min and ' + (uptime % 60) + ' seconds'
     } else{
         return (uptime % 60) + ' seconds';
     }
